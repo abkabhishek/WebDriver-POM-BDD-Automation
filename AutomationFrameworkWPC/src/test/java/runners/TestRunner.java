df921759139d7507c1ab5fde05110645b5f892b5
Abhishek Kumar <abkabhi>
2018-09-29 00:42:22 +0530

    Added Extent Report

--- AutomationFrameworkWPC/src/test/java/runners/TestRunner.java
+++ AutomationFrameworkWPC/src/test/java/runners/TestRunner.java
@@ -1,6 +1,14 @@
 package runners;
 
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+
+//import cucumber.listener.Reporter;
+import com.vimalselvam.cucumber.listener.Reporter;
+
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -10,9 +18,17 @@ import cucumber.api.junit.Cucumber;
 		features = "src/test/resources/functionalTests",
 		glue= {"stepDefinitions"},
 		dryRun=false,
-		plugin = { "pretty"}, //"html:Reports/cucumber-reports" },
+//		plugin = { "pretty"}, //"html:Reports/cucumber-reports" },
+		plugin = { "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
 		monochrome = true
 		)
 public class TestRunner {
+	
+	
+	
+	@AfterClass
+	public static void writeExtentReport() {
+		Reporter.loadXMLConfig(new File("config/extent-config.xml"));
+	}
 
 }
--- AutomationFrameworkWPC/src/test/java/services/Log.java
+++ AutomationFrameworkWPC/src/test/java/services/Log.java
@@ -2,6 +2,7 @@ package services;
 
 import java.io.File;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 
@@ -11,7 +12,10 @@ public class Log {
 	
 	public static final Logger Log = Logger.getLogger("devLogger");
 	
+	
+	
 	public Log() {
+		BasicConfigurator.configure();
 		Log.error("Hello");
 		Log.error("Super");
 	}
